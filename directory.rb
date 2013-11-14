839f0b647ce6bf88d5680a071f432002b3ee562d
kenmasco <kennerdeigh@me.com>
2013-11-14 18:11:36 +0000

    Added hobies input

--- directory.rb
+++ directory.rb
@@ -12,10 +12,16 @@ def input_students
 		puts "Please enter the cohort month of the student"
 		# get the cohort
 		cohort = gets.chomp
+		# while the cohort is not empty, repeat this code
+		# get hobbies
 		break if cohort.empty?
+		puts "Please enter your favourite pastimes"
+		#get the hobies from the user
+		hobbies = gets.chomp
+		break if hobbies.empty?
 		# while the cohort is not empty, repeat this code
 		#add the student hash to the array
-		students << { :name => capitalize_all(name), :cohort => cohort.capitalize}
+		students << { :name => capitalize_all(name), :cohort => cohort.capitalize, :hobbies => hobbies}
 	end
 	#return the array of students
 	students
@@ -32,16 +38,16 @@ def print_header
 end
 
 def display(students)
-	puts "Names Beginning with 'A'\n===========================".center(150) if students.any? {|student| student[:name][0] == 'A'}
+	puts "Names beginning with 'A'".center(150) + "\n" + "========================".center(150) if students.any? {|student| student[:name][0] == 'A'}
 	students.each_with_index do |student, i|
 		if student[:name][0] == "A" 
-			puts "#{i+1}: #{student[:name]} (#{student[:cohort]} cohort)".center(150)
+			puts "#{i+1}: #{student[:name]} (#{student[:cohort]} Cohort\n)".center(150)
 		end
 	end
-	puts "Names Not Beginning with 'A'\n===========================".center(150) if students.any? {|student| student[:name][0] != 'A'}
+	puts "Names not beginning with 'A'".center(150) + "\n" + "========================".center(150) if students.any? {|student| student[:name][0] != 'A'}
 	students.each_with_index do |student, i|
 		if student[:name][0] != "A" 
-			puts "#{i+1}: #{student[:name]} (#{student[:cohort]} cohort)".center(150)
+			puts "#{i+1}: #{student[:name]} (#{student[:cohort]} Cohort\n)".center(150)
 		end
 	end
 
